372e79caaf029026f880c8335213ee83e87dfe2a
NFTGameA <joehirest72@gmail.com>
2023-11-26 18:21:13 +0700

    fix

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,10 +1,10 @@
 // app/api/img/route.ts
 export async function GET(request: Request) {
     return new Response(JSON.stringify({
-      name: "TokenN",
-      symbol: "FUD",
-      description: "Just a test for how to name your token ;)",
-      image: "https://dataapi-f8d3b.web.app/usdc.jpg"
+      name: "Konk",
+      symbol: "KONK",
+      description: "KONK represent a unique blend of internet culture and digital finance.",
+      image: "https://dataapi-f8d3b.web.app/konk.jpg"
     }), {
       headers: {
         'Content-Type': 'application/json'
