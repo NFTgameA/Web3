fe78d3aa467c367efc70c1990105dd0e4b7d683c
NFTGameA <joehirest72@gmail.com>
2023-11-26 21:28:07 +0700

    fix

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,7 @@
 // app/api/img/route.ts
 export async function GET(request: Request) {
     return new Response(JSON.stringify({
-      name: "Baby Ponk",
-      symbol: "PONK",
+
       description: "BABYPONK represent a unique blend of internet culture and digital finance",
       image: "https://dataapi-f8d3b.web.app/konk.jpg"
     }), {
